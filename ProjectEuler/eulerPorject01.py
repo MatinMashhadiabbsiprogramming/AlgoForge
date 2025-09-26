21c2646b9f30d8fbb532151516b4faaf7107d3c0
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-09-27 01:04:07 +0330

    euler project 1 is solved

--- ProjectEuler/eulerPorject01.py
+++ ProjectEuler/eulerPorject01.py
@@ -1,2 +1,13 @@
 # problem 1 | Multiples of 3 or 5 
 
+value = 1000 
+count=0
+listMutiples=[]
+
+for i in range(1,value):
+    if (i%3 ==0 ) or (i%5==0):
+        count+=i
+        listMutiples.append(i)
+        
+print(f'list of multiples of 3 and 5 is:{listMutiples}')
+print(f"sum of mutiples is : {count}")
