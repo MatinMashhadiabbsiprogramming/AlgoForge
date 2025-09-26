5f89d6fe7374ffe0f018fc907f5b273b8a3f941e
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-09-27 01:12:31 +0330

    euler project 2 is solved

--- ProjectEuler/eulerProjcet02.py
+++ ProjectEuler/eulerProjcet02.py
@@ -1,1 +1,12 @@
 # problem 2 | Even Fibonacci Numbers
+
+continueS = 4000000
+a=1
+b=2
+Sum_fib_Even=0
+while a<= continueS:
+    if a %2 ==0 :
+        Sum_fib_Even +=a
+    a,b=b,a+b
+    
+print("sum of Even fibonacci numbers 4mil",Sum_fib_Even)
