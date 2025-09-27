c51872bcaf1993fda4dbe71793d9ed832c617734
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-09-28 01:01:28 +0330

    quera Challenge ReversePrint ==> 3405 is solved

--- Quera/ReversePrint.py
+++ Quera/ReversePrint.py
@@ -1,1 +1,13 @@
 #3405
+
+listNumbers=[]
+
+while True:
+    a=int(input())
+    if a!=0:
+        listNumbers.append(a)
+    else:
+        break
+
+for i in range(len(listNumbers)-1,-1,-1):
+    print(listNumbers[i])
