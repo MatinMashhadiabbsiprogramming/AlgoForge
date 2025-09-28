aa2b8c455f19ce0942144f95ebd6c2375410d55f
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-09-28 19:37:09 +0330

    euler project 6 is solved

--- ProjectEuler/eulerProject06.py
+++ ProjectEuler/eulerProject06.py
@@ -1,1 +1,18 @@
 # Problem 6 | Sum Square Difference
+
+sum_quares=0
+square_Sum=0
+for i in range(1,101):
+    sum_quares+=i**2
+    square_Sum+=i
+else:
+    square_Sum=square_Sum**2
+    print(i)
+    print(square_Sum)
+
+print(sum_quares)
+result=(square_Sum - sum_quares) 
+print(result)
+
+
+
