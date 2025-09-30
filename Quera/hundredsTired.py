626d9a1babe5c1450af44760291f954270a05768
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-10-01 01:37:37 +0330

    quera Chellenge hundreds of Tired ==> (3406) is hard Solved

--- Quera/hundredsTired.py
+++ Quera/hundredsTired.py
@@ -1,1 +1,15 @@
 #3406
+
+a=input()
+b=input()
+
+repa=a[::-1]
+repb=b[::-1]
+if int(repa)==int(repb):
+    print(f"{a} = {b}")
+    exit()
+if int(repa) < int(repb):
+    print(f"{a} < {b}")
+else:
+    print(f"{b} < {a}")
+    
