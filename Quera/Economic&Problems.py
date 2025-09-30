c3ed5b8d60c36539578f20adb519ee5031a496e5
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-10-01 02:01:44 +0330

    quera Challenge(Economic Problems) ==>(20249) is solved

--- Quera/Economic&Problems.py
+++ Quera/Economic&Problems.py
@@ -1,1 +1,18 @@
 #20249
+
+n,k=map(int,input().split())
+adad=list(map(int,input().split()))
+
+sums=sum(adad)
+
+# solutins is Time Limit Exceeded
+sum2=n+k
+while sums!=0: 
+    sums -=k 
+    n-=1
+
+# this solutins is Time Limit Exceeded is solved:
+# solutins ai
+##stepes=(sums+k-1)//k
+##n-=stepes
+print(n)
