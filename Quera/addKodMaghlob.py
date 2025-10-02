c77df8809de12fb7a35e7701af2c6e297d5b19f6
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-10-03 00:51:17 +0330

    quera Challenge addKodMaghlob ==> 617 is solved

--- Quera/addKodMaghlob.py
+++ Quera/addKodMaghlob.py
@@ -1,1 +1,10 @@
-#617 
+#617
+numInput=input()
+result=''
+for i in range(len(numInput)-1,-1,-1) :
+    result+=numInput[i] 
+else:
+    if int(numInput) == int(result):
+        print('YES')
+    else:
+        print('NO') 
