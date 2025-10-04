0a9d884d6f014ba81dd0d22b9127c9663c945645
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-10-04 19:58:40 +0330

    quera Challenge Pythagorean Numbers ==> 280 is solved

--- Quera/PythagoreanNumbers.py
+++ Quera/PythagoreanNumbers.py
@@ -1,1 +1,13 @@
 # 280
+a=int(input())
+b=int(input())
+c=int(input())
+
+if a!=0 and b!=0 and c!=0:
+    x,y,z=sorted([a,b,c])
+    if x**2 + y**2 == z**2: 
+        print("YES")
+    else:
+        print("NO")
+else:
+    print("NO")
