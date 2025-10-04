a8affe32848629e40d2e4d74d8c75c502c9fd4f4
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-10-04 18:18:46 +0330

    quera Challenge Mabbna ==> 594 is solved

--- Quera/Mabbna.py
+++ Quera/Mabbna.py
@@ -1,1 +1,22 @@
 # 594
+a,b=map(int,input().split())
+
+def ConvertNum(N1,N2):
+    result = ''
+    while N2<N1:
+        result+=str(N1%N2)
+        N1=N1 // N2
+    result+=str(N1)
+    return result[::-1]
+objects=(ConvertNum(a,b))
+sum1=0
+sum2=0
+for i in objects[::2]:
+    sum1+=int(i)
+for i in objects[1::2]:
+    sum2+=int(i)
+if sum1 == sum2:
+    print("Yes")
+else: 
+    print("No")
+# print(sum1,sum2)
