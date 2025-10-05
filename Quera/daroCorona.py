5b268724dd71fd8d555b1f872b5eb1f4da07aa43
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-10-05 23:45:14 +0330

    quera Challenge daroCorona ==> 178600 is solved

--- Quera/daroCorona.py
+++ Quera/daroCorona.py
@@ -1,1 +1,19 @@
 #178600
+n=int(input())
+k=int(input())
+p=int(input())
+q=int(input())
+
+Shekarestan=n-k
+Namakestan=p-q
+
+
+if Shekarestan == Namakestan:
+    print("Equal")
+
+if Shekarestan > Namakestan:
+    print("Shekarestan")
+
+if Shekarestan < Namakestan:
+    print("Namakestan")
+    
