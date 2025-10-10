b2bc6bec2a5aa6e51a766553b4ef1751f066bba2
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-10-10 19:29:04 +0330

    quera Challenge darMetro ==> 218361 is solved

--- Quera/darMetro.py
+++ Quera/darMetro.py
@@ -1,1 +1,11 @@
 # 218361
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+
+counter=0
+
+for i in range(8):
+    if a[i] == 1 and b[i]==1:
+        counter+=1
+        continue
+print(counter)
