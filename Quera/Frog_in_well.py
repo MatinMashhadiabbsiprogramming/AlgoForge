ca80f7fb23a7e0e5df59def83bb7ac658c78dfa7
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-10-10 12:47:16 +0330

    quera Challenge Frog in the Well ==> 235327 is solved

--- Quera/Frog_in_well.py
+++ Quera/Frog_in_well.py
@@ -1,1 +1,21 @@
 # 235327
+
+t=int(input())
+res=[]
+for i in range(t):
+    a,b,h=map(int,input().split())
+    
+    if a>=h: 
+        res.append(1)
+        continue
+    
+    c=0
+    days=0
+    while True:
+        days+=1
+        c+=a
+        if c>=h:
+            res.append(days)
+            break
+        c-=b
+print("\n".join(map(str,res)))
