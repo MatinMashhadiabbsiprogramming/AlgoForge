b81a730b4342b20a917f14fcc91f432ef1cf6a7f
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-10-11 23:28:44 +0330

    leetCode Challenge(58.Length of Last Word) is solved

--- leetCode/58.LengthofLastWord.py
+++ leetCode/58.LengthofLastWord.py
@@ -1,1 +1,19 @@
 # 58. Length of Last Word
+
+# sorc leet Code 
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        listText=s.split()
+        lastValue=listText[-1]
+        return len(lastValue)
+    
+# my test
+class Solution2(object):
+    def lengthOfLastWord2(self, s):
+        listText=s.split()
+        return len(listText[-1])
+  
+txt="this is My information"  
+obj=Solution2()
+obj2=obj.lengthOfLastWord2(txt)
+print(obj2)
