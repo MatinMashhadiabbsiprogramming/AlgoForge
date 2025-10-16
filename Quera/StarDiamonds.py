240774f1b94dd971e13362d2040a4a83d470a54b
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-10-16 17:32:55 +0330

    quera Challenge Star diamonds ==> 9773 is  solved

--- Quera/StarDiamonds.py
+++ Quera/StarDiamonds.py
@@ -1,1 +1,18 @@
 # 9773
+n=int(input())
+tu=1
+for i in range(1,n+1,2):
+    
+    s='*'*i
+    # result=' '*(int((n/2))-tu)+ s+' '*((n-(2*i+1))//2)+' '*(int((n/2))-tu) + s
+    result=' '*(int((n/2))-tu)+ s+' '*(int((n/2)+1)-tu)
+    result = result*2
+    print(result)
+    tu+=1 
+tu2=1
+for i in range(n-2,-1,-2):
+    s='*'*i
+    result=' '*((tu2)-1)+ s+' '*(tu2)
+    result = result*2
+    print(result)
+    tu2+=1
