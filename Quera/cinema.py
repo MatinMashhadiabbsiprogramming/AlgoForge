e912122a02b081a152296972aa672a8c635cef8d
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-11-27 23:24:28 +0330

    quera Challenge Cinema ==> 235328 is solved

--- Quera/cinema.py
+++ Quera/cinema.py
@@ -1,1 +1,21 @@
 # 235328
+
+n,k=map(int,input().split())
+friend=list(map(int,input().split()))
+
+g_size=[1 + x for x in friend]
+print(g_size)
+g_size.sort()
+print(g_size)
+
+total = 0
+count = 0
+
+for i in range(n):
+    if total + g_size[i] <= k :
+        total += g_size[i]
+        count+=1
+    else:
+        break
+
+print(count)
