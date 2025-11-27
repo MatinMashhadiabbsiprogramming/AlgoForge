1673677a1a0316c4999bc1d68571dcbb7cbe0991
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2025-11-27 23:49:12 +0330

    quera Challenge Square ==>283 is Solved

--- Quera/Square.py
+++ Quera/Square.py
@@ -1,1 +1,21 @@
 #283
+
+a = int(input())
+b = int(input())
+if a<=b:
+    print("Wrong order!")
+
+if (a-b)%2!=0:
+    print("Wrong difference!")
+    
+else:
+    start = (a - b) // 2  
+    end = start + b      
+
+    for i in range(a):
+        for j in range(a):
+            if start <= i < end and start <= j < end:
+                print(" ", end=" ")  
+            else:
+                print("*", end=" ")  
+        print()  
