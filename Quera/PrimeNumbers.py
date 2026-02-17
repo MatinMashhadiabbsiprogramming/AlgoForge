8627ba53b11ad21f51fb1eb6ae86b1cc5480761f
MatinMashhadiabbasiprogramming <matinabbasi804@gmail.com>
2026-02-17 04:22:20 +0330

    quera Challenge Prime Numbers ==> 293 is Solved

--- Quera/PrimeNumbers.py
+++ Quera/PrimeNumbers.py
@@ -1,1 +1,14 @@
 # 293 
+
+a=int(input())
+b=int(input())
+
+for i in range(a,b+1):
+    if i < 2  : 
+        continue
+    prime=True
+    for j in range(2,i):
+        if i % j ==0 : 
+            prime= False
+            break
+    if prime: print(i)
